c861e0f5b6007d74b06f5913a8ca0f1f12752144
lamloi1109 <lamloi12a1@gmail.com>
2023-05-03 20:59:30 +0700

    update

--- Day9/lv3.js
+++ Day9/lv3.js
@@ -49,3 +49,68 @@ function findPopulatedCountries(countries, num) {
 }
 console.log(findPopulatedCountries(countries, 10));
 // *** Try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create an object called statistics and create all the functions which do statistical calculations as method for the statistics object. Check the output below.
+const ages = [
+  31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37,
+  31, 34, 24, 33, 29, 26,
+];
+console.log(ages.length);
+const statistics = {
+  count: function () {
+    return ages.length;
+  },
+  sum: function () {
+    return ages.reduce((acc, curr) => acc + curr);
+  },
+  max: function () {
+    return ages.sort((a, b) => b - a)[0];
+  },
+  min: function () {
+    return ages.sort((a, b) => a - b)[0];
+  },
+  range: function () {
+    return this.max() - this.min();
+  },
+  mean: function () {
+    return Math.round(this.sum() / this.count());
+  },
+  median: function () {
+    // Trung bình của 2 số ở giữa trong tập dữ liệu tăng dần
+    // https://vietnambiz.vn/trung-vi-median-la-gi-vi-du-ve-trung-vi-2019110713491368.htm
+    const getMiddleIndex = Math.floor(this.count() / 2);
+    return Math.round((ages[getMiddleIndex - 1] + ages[getMiddleIndex]) / 2);
+  },
+  mode: function () {},
+  var: function () {
+    const average = this.mean();
+    const squareDiffs = ages.map((value) => {
+      const diff = value - average;
+      return diff * diff;
+    });
+    const variance =
+      squareDiffs.reduce((acc, curr) => acc + curr) / squareDiffs.length;
+    // (X) = E (x – μ) ² / N
+    //     x là giá trị trong tập dữ liệu
+
+    // µ là giá trị trung bình của tập dữ liệu
+
+    // n là số lượng giá trị trong tập dữ liệu
+    // https://www.slingacademy.com/article/calculate-variance-and-standard-deviation-in-javascript/#:~:text=map((value%3A%20number)%3A,number%20%3D%3E%20%7B%20return%20Math.
+    return variance;
+  },
+  std: function () {
+    return Math.sqrt(this.var());
+  },
+  freqDist: function () {},
+};
+
+console.log("Count:", statistics.count()); // 25
+console.log("Sum: ", statistics.sum()); // 744
+console.log("Min: ", statistics.min()); // 24
+console.log("Max: ", statistics.max()); // 38
+console.log("Range: ", statistics.range()); // 14
+console.log("Mean: ", statistics.mean()); // 30
+console.log("Median: ", statistics.median()); // 29
+// console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+console.log("Variance: ", statistics.var()); // 17.5
+console.log("Standard Deviation: ", statistics.std()); // 4.2
+console.log("Variance: ", statistics.var()); // 17.5
